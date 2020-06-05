79493ab5151aee9d3ca032facc2b631ef926982a
Truong Thanh Tung <ttungbmt@gmail.com>
2020-06-05 15:38:37 +0700

    Update GeomValidator.php

--- src/validators/GeomValidator.php
+++ src/validators/GeomValidator.php
@@ -1,36 +1,70 @@
 <?php
+
 namespace ttungbmt\validators;
 
 use yii\base\DynamicModel;
 use yii\validators\EachValidator;
 use yii\validators\Validator;
 
-class GeomValidator extends Validator {
-    public function validateAttribute($model, $attribute) {
-        $mesage = 'Toạ độ không hợp lệ';
-        $valid1 = new EachValidator(['rule' => ['number']]);
-        $value = collect($model->$attribute)->filter(function ($i){
-            return ($i || $i == 0) && trim($i) !== "" ;
+class GeomValidator extends Validator
+{
+    /**
+     * @var boolean whether the attribute value can be null or empty. Defaults to false.
+     * If this is true, it means the attribute is considered valid when it is empty.
+     */
+    public $allowEmpty = true;
+
+    public $geoprocessing;
+    public $geometry;
+
+    public function validateAttribute($model, $attribute)
+    {
+        $value = collect($model->$attribute)->filter(function ($i) {
+            return ($i || $i == 0) && trim($i) !== "";
         });
 
-        if($value->count() === 0) {
+        if ($this->allowEmpty && $this->isEmpty($value)) {
             $model->$attribute = null;
-            return;
+            return null;
         }
 
-        if($value->count() === 2 && $valid1->validate($value->all())){
-            $valid2 = DynamicModel::validateData(['lat' => $value->get('1'), 'lng' => $value->get('0')], [
-                ['lat', 'match', 'pattern' => '/^(\+|-)?(?:90(?:(?:\.0{1,15})?)|(?:[0-9]|[1-8][0-9])(?:(?:\.[0-9]{1,15})?))$/'],
-                ['lng', 'match', 'pattern' => '/^(\+|-)?(?:180(?:(?:\.0{1,15})?)|(?:[0-9]|[1-9][0-9]|1[0-7][0-9])(?:(?:\.[0-9]{1,15})?))$/'],
-            ]);
 
-            if(!$valid2->hasErrors()){
-                $model->$attribute = $value->sortKeys()->all();
-                return;
-            }
-            $this->addError($model, $attribute, collect($valid2->getErrors())->flatten()->implode(', '));
+        if (!$this->validateLatLng($value, [$model, $attribute])) {
+            return false;
         }
 
-        $this->addError($model, $attribute, $mesage);
+        if($this->geoprocessing && !$this->validateGeoprocessing()){
+            return false;
+        }
+
     }
+
+
+    public function validateLatLng($value, array $params)
+    {
+        $validator = DynamicModel::validateData(['lat' => $value->get('1'), 'lng' => $value->get('0')], [
+            ['lat', 'match', 'pattern' => '/^(\+|-)?(?:90(?:(?:\.0{1,15})?)|(?:[0-9]|[1-8][0-9])(?:(?:\.[0-9]{1,15})?))$/'],
+            ['lng', 'match', 'pattern' => '/^(\+|-)?(?:180(?:(?:\.0{1,15})?)|(?:[0-9]|[1-9][0-9]|1[0-7][0-9])(?:(?:\.[0-9]{1,15})?))$/'],
+        ]);
+
+        if ($value->count() === 2 && !$validator->hasErrors()) {
+            return true;
+        }
+
+        $this->addError($params[0], $params[1], $this->implodeMessage($validator));
+    }
+
+    public function implodeMessage($validator)
+    {
+        return collect($validator->getErrors())->flatten()->implode(', ');
+    }
+
+    public function validateGeoprocessing()
+    {
+        if(is_array($this->geoprocessing)){
+            return call_user_func($this->geoprocessing);
+        }
+    }
+
+
 }
